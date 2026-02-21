71cd81e8963ead6e18264dbdf9a57547e5fbece0
Daniel Kubec <n13l@rtfm.cz>
2026-02-21 16:25:59 +0000

    Added constant-time, branchless jump to inlined digest.

--- crypto/digest.h
+++ crypto/digest.h
@@ -2,10 +2,11 @@
 #define __OSS_CRYPTO_DIGEST_H__
 
 #include <hpc/compiler.h>
+#include <hpc/array.h>
 #include <hpc/mem/unaligned.h>
 #include <crypto/digest/sha3.h>
 
-enum digest_e {
+enum digest_algorithm {
 	SHA3_224 = 1,
 	SHA3_256 = 2,
 	SHA3_384 = 3,
@@ -13,23 +14,9 @@ enum digest_e {
 };
 
 struct digest {
+	enum digest_algorithm algo;
 	u8 data[512];
 };
-
-static inline void
-digest_init(struct digest *digest, enum digest_e algo)
-{
-}
-
-static inline void
-digest_update(struct digest *digest, const u8 *data, unsigned int len)
-{
-}
-
-static inline void
-digest_final(struct digest *digest, u8 *out)
-{
-}
 
 static inline void
 sha3_224_init(struct sha3 *sha3)
@@ -66,5 +53,33 @@ sha3_final(struct sha3 *sha3, u8 *out)
 {
 	arch_sha3_final(sha3, out);
 }
+
+static inline void
+digest_init(struct digest *digest, enum digest_algorithm algo)
+{
+	digest->algo = algo;
+}
+
+static inline void
+digest_update(struct digest *digest, const u8 *data, unsigned int len)
+{
+	STATIC_ARRAY_STREAMLINED(void *, dispatch, &&digest_undef,
+		[SHA3_256] = &&sha3_256
+	);
+
+	goto ARRAY_STREAMLINED_AT(dispatch, digest->algo);
+
+sha3_256:
+	sha3_update((struct sha3 *)digest, data, len);
+	return;
+digest_undef:
+	return;
+}
+
+static inline void
+digest_final(struct digest *digest, u8 *out)
+{
+}
+
 
 #endif
